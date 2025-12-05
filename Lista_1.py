4ea3f000ee8f5ec4196439db483967a7be3a4a88
Juan Felipe <juanfelipesilvadesouza@gmail.com>
2025-12-05 18:04:14 -0300

    Add files via upload

--- Lista_1.py
+++ Lista_1.py
@@ -55,3 +55,109 @@ elif cor == "Amarelo":
     print("R$ 30.00")
 elif cor == "Vermelho":
     print("R$ 40.00")
+
+# Escreva um programa que recebe três números e retorna a soma deles, porém se
+# houver números repetidos o valor deles não é contabilizado. Por exemplo, na entrada
+# (1,2,3) a resposta é 6, na entrada (3,2,3) a resposta é 2 e na entrada (3,3,3) a resposta
+# é 0.    
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+soma = 0
+
+if a != b and a != c:
+    soma = soma + a
+
+if b != a and b != c:
+    soma = soma + b
+
+if c != a and c != b:
+    soma = soma + c
+
+print(soma)
+
+# Escreva um programa que receba três números. O programa deve imprimir a palavra
+# “soma” se a soma de dois deles for igual ao outro número, caso contrário, o programa
+# deve imprimir a palavra “multi” se a multiplicação de dois deles for igual ao outro
+# número. Caso nenhuma das duas possibilidades for verdade, então imprimir a palavra
+# “par” se a soma dos três números for par, e imprimir a palavra “impar” caso contrário.
+# Por exemplo, na entrada (8,3,5) a resposta é “soma”, na entrada (3,3,1) a resposta é
+# “multi”, na entrada (8,4,9) a resposta é “impar”.
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+aux = a + b + c
+
+if a + b == c or a + c == b or b + c == a:
+    print("Soma")
+
+elif a * b == c or a * c == b or b * c == a:
+    print("Multi")
+
+elif aux % 2 == 0:
+    print("Par")
+
+else:
+    print("Impar")
+
+# indique se formam um
+# triângulo, juntamente com o seu tipo (equilátero, isósceles e escaleno):
+
+# i. Equilátero: todos os lados iguais
+# ii. Isósceles: dois lados iguais
+# iii. Escaleno: todos os lados diferentes
+
+a = int(input("Num1:"))
+b = int(input("Num2:"))
+c = int(input("Num3:"))
+
+cond_exist = a + b > c and b + c > a and a + c > b
+
+if cond_exist == False:
+    print("Sem Triangulo")
+
+else:
+
+    if a != b and a != c and b != c:
+        print("Triangulo Escaleno")
+
+    elif a == b and a == c:
+        print("Triangulo Equilátero")
+
+    else:
+        print("Triangulo Isósceles")
+
+# Faça um programa que a partir de um número informado em centavos (inteiro), indique
+# a menor quantidade de moedas que representa esse valor. Considere moedas de 1,25
+# e 50 centavos e 1 real (100 centavos). Exemplo: 290 centavos é representado por 2
+# moedas de 1 real, 1 de 50c, 1 de 25c, 15 de 1c.
+# Dica) Podemos usar as operações de divisão inteira (//) e resto da divisão (%) para
+# saber quantas moedas de um tipo podem ser usadas no troco.
+# Ex: Para um valor de 142 centavos e a moeda de 25 centavos, temos que 142//25=5,
+# logo podemos dar 5 moedas de 25 centavos no troco. Além disso, veja que
+# 142%25=17, logo depois de dar o troco de 5 moedas de 25 centavos, ainda restaria 17
+# centavos para dar de troco em moedas de valor menor.
+
+valor_total = int(input("Digite o valor em centavos: "))
+
+moedas_100 = valor_total // 100
+resto = valor_total % 100
+
+moedas_50 = resto // 50
+resto = resto % 50
+
+moedas_25 = resto // 25
+resto = resto % 25
+
+moedas_1 = resto
+
+
+print("--- Resultado ---")
+print(f"Moedas de 1 real: {moedas_100}")
+print(f"Moedas de 50 centavos: {moedas_50}")
+print(f"Moedas de 25 centavos: {moedas_25}")
+print(f"Moedas de 1 centavo: {moedas_1}")
